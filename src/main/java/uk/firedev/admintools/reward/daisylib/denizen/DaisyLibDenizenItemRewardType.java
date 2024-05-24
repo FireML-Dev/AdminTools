5ecf582dbd5c01dc042c450c5a76e160ae8328f1
FireML <106587317+FireML@users.noreply.github.com>
2024-05-24 08:35:17 +0100

    Logger updates

--- src/main/java/uk/firedev/admintools/reward/daisylib/denizen/DaisyLibDenizenItemRewardType.java
+++ src/main/java/uk/firedev/admintools/reward/daisylib/denizen/DaisyLibDenizenItemRewardType.java
@@ -19,13 +19,13 @@ public class DaisyLibDenizenItemRewardType implements uk.firedev.daisylib.reward
         String[] splitValue = value.split(",");
         ItemTag itemTag = ItemTag.valueOf(splitValue[0], false);
         if (itemTag == null) {
-            Loggers.log(Level.INFO, getPlugin().getLogger(), "Invalid Denizen Item specified for RewardType " + getIdentifier() + ": " + splitValue[0]);
+            Loggers.info(getComponentLogger(), "Invalid Denizen Item specified for RewardType " + getIdentifier() + ": " + splitValue[0]);
             return;
         }
         int quantity = 1;
         if (splitValue.length >= 2) {
             if (!ObjectUtils.isInt(splitValue[1])) {
-                Loggers.log(Level.INFO, getPlugin().getLogger(), "Invalid number specified for RewardType " + getIdentifier() + ": " + splitValue[1]);
+                Loggers.info(getComponentLogger(), "Invalid number specified for RewardType " + getIdentifier() + ": " + splitValue[1]);
                 return;
             }
             quantity = Math.max(Integer.parseInt(splitValue[1]), 1);
