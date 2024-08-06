e90f4225ededf0fd2a21c364fe46a285b318eab9
FireML <firemlyt@gmail.com>
2024-08-06 09:54:06 +0100

    Migrate to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,116 @@
+import net.minecrell.pluginyml.paper.PaperPluginDescription
+
+plugins {
+    `java-library`
+    `maven-publish`
+    alias(libs.plugins.plugin.yml)
+    alias(libs.plugins.shadow)
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    gradlePluginPortal()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.william278.net/releases/")
+    maven("https://repo.firedev.uk/repository/maven-public/")
+    maven("https://maven.citizensnpcs.co/repo/")
+}
+
+dependencies {
+    compileOnly(libs.paper.api)
+    compileOnly(libs.cmi.api)
+    compileOnly(files("$projectDir/libs/even-more-fish-1.7.jar"))
+    compileOnly(libs.denizen.api)
+    compileOnly(libs.multiverse)
+    compileOnly(files("$projectDir/libs/Jobs5.2.3.1.jar"))
+    compileOnly(files("$projectDir/libs/CMILib1.4.7.16.jar"))
+    compileOnly(libs.daisylib)
+}
+
+group = "uk.firedev"
+version = "1.0.2-SNAPSHOT"
+description = "Tools to help out Admins and Devs"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+paper {
+    name = project.name
+    version = project.version.toString()
+    main = "uk.firedev.admintools.AdminTools"
+    apiVersion = "1.21"
+    author = "FireML"
+    description = project.description.toString()
+    foliaSupported = true
+
+    serverDependencies {
+        register("DaisyLib") {
+            required = true
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+        register("EvenMoreFish") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+        register("Multiverse-Core") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+        register("Denizen") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+        register("Jobs") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "firedevRepo"
+
+            // Repository settings
+            var repoUrlString = "https://repo.firedev.uk/repository/maven-"
+            repoUrlString += if (project.version.toString().endsWith("-SNAPSHOT")) {
+                "snapshots/"
+            } else {
+                "releases/"
+            }
+            url = uri(repoUrlString)
+
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        archiveBaseName.set(project.name)
+        archiveVersion.set(project.version.toString())
+        archiveClassifier.set("")
+
+        manifest {
+            attributes["paperweight-mappings-namespace"] = "spigot"
+        }
+    }
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "AdminTools"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("paper-api", "io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+            library("daisylib", "uk.firedev:DaisyLib:2.0.3-SNAPSHOT")
+            library("cmi-api", "CMI-API:CMI-API:9.7.0.1")
+            library("evenmorefish", "com.oheers:EvenMoreFish:1.7")
+            library("denizen-api", "com.denizenscript:denizen:1.3.1-SNAPSHOT")
+            library("multiverse", "com.onarandombox.multiversecore:multiverse-core:4.3.12")
+            library("jobs", "Jobs:jobs:5.2.3.1")
+            library("cmilib", "CMILib:CMILib:1.4.7.16")
+
+            plugin("plugin-yml", "net.minecrell.plugin-yml.paper").version("0.6.0")
+            plugin("shadow", "io.github.goooler.shadow").version("8.1.8")
+        }
+    }
+}
